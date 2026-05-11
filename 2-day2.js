c0047f46f772b463bd7d5b7e3948cda899073ab4
Dipanshu-26 <dnchawde@gmail.com>
2026-05-11 21:56:34 +0530

    first commit

---
+++ 2-day2.js
@@ -0,0 +1,37 @@
+//js is dynamically typed language 
+
+let a =10 
+console.log(a)
+console.log(typeof(a))
+
+a=20.4
+console.log(a)
+console.log(typeof(a))
+
+a=-20.4
+console.log(a)
+console.log(typeof(a))
+//10,10.5,-10,-10.5 == number
+
+a="javascript"
+console.log(a)
+console.log(typeof(a))
+
+a='dip@123'
+console.log(a)
+console.log(typeof(a))
+
+a="234"
+console.log(a)
+console.log(typeof(a))
+
+a='@#$'
+console.log(a)
+console.log(typeof(a))
+//"dipanshu","dip@123", "123",'@#$'
+
+a= false
+console.log(a)
+console.log(typeof(a))
+//true , false ==> boolean
+
