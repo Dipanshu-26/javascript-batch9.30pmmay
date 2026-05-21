bc839c05d3543c1e9319be0a9a6b9fe15592fbd6
Dipanshu-26 <dnchawde@gmail.com>
2026-05-21 22:01:44 +0530

    21-May

---
+++ 10-loops.js
@@ -0,0 +1,87 @@
+//loops in javascript
+
+//hello 3 times 
+
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+
+// //hello 50 times 
+
+// // 1 to 10 
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+//print hello 3 time 
+
+// for loop, while 
+
+// for 
+// syntax 
+// for (inilization; condition; inc/dec)
+
+// for(let i =1 ; i<=3 ; i++){      //i++ = => i=i+1     i =1 ,2,3,4
+//     console.log("hello")         //hello ,hello
+// }
+
+// //print 1 to 10
+
+// for(let x=1;x<=10;x++){       //x= 1,2,3....10,11
+//     console.log(x)           // 1,2,3.....10
+// }
+
+//print 0 to 10
+// for(let x=0;x<=10;x++){       //x=0, 1,2,3....10,11
+//     console.log(x)           // 0,1,2,3.....10
+// }
+
+//print table of 2
+
+// for(let i=2;i<=20;i=i+2){
+//     console.log(i)
+// }
+
+//-----------------------------------------
+// for(let i=1;i<=10;i++){         //i=1,2,3.....10,11
+//     console.log(i*2)            //1*2 = 2,4,6.....20
+// }
+
+
+// //print table of 5 
+// for(let i=5;i<=50;i=i+5){
+//     console.log(i)
+// }
+
+// //--------------------------------------------
+// //print 0 to 10 in reverse
+
+// for(let i=10;i>=0;i--){         //i=10,9,8.....0,-1
+//     console.log(i)              //10,9,8.....0
+// }
+
+// //--------------------------------------------
+// //print 1 to 10 in reverse
+
+// for(let i=10;i>0;i--){         //i=10,9,8.....1,0     
+//     console.log(i)              //10,9,8.....1
+// }
+
+// // i>=0    ==>0 
+// // i>0    ==>1 
+
+//--------------------------------------------
+//print table of 2 in reverse
+// for(let i=20;i>=2;i=i-2){
+//     console.log(i)
+// }
+
+
+//--------------------------------------------
+//print table of 2 in reverse
+for(let i=10;i>=1;i--){
+    console.log(i*2)
+}
+
+//print table of 5 in reverse
+
