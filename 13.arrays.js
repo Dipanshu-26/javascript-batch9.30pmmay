e4f95b40327096cb9447b391f33dd7350fcd3c89
Dipanshu-26 <dnchawde@gmail.com>
2026-05-26 21:52:36 +0530

    26-May

---
+++ 13.arrays.js
@@ -0,0 +1,32 @@
+//array in JS
+//An array is a list used to store multiple values in a single variable.
+//values of array accessed through index  (starts from 0)
+let a =20
+let b = "dipanshu"
+
+
+//            0           1       2        3
+let arr1 = ["dipanshu","nitin","tanish","neel"]     //length =4 index of last element = 3 (length-1)
+
+console.log(arr1)
+console.log(arr1[0])
+console.log(arr1[1])
+console.log(arr1[2])
+console.log(arr1[3])
+
+console.log(typeof(arr1))
+
+//java script is dynamically typed language
+//object 
+//prperties 
+//methods
+
+console.log(arr1.length)
+//             0  1  2   3  4
+let numbers = [11,22,33,44,55]
+console.log(numbers)
+console.log(numbers[3])
+
+let info = ["dipanshu","chawde",23,56,true]
+console.log(info[0])
+console.log(info[4])
