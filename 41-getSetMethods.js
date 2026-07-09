295c2072936080311d6acc1be1f7c744173a187a
Dipanshu-26 <dnchawde@gmail.com>
2026-07-09 21:49:01 +0530

    9-july

---
+++ 41-getSetMethods.js
@@ -0,0 +1,49 @@
+class Person {
+    setFirstName(fn){
+        this.fname = fn
+    }
+
+    setLastName(ln){
+        this.lname = ln
+    }
+
+    getFirstName(){
+        return this.fname
+    } 
+
+    getLastName(){
+        return this.lname
+    }
+
+}
+
+let dip = new Person()
+dip.setFirstName("dipanshu")
+console.log(dip.getFirstName())
+// dip.fname = "neel"
+// console.log(dip.getFirstName())
+
+//--------------------------------------------------------------------
+//get set keywords 
+
+class PersonB{
+    set firstName(fn){
+        this.fname=fn
+    }
+    set lastName(ln){
+        this.lname=ln
+    }
+    get lastN(){
+        return this.lname
+    }
+    get firstN(){
+        return this.fname
+    }  
+}
+
+let adi = new PersonB()
+adi.firstName = "aditya"
+adi.lastName = "masalkar"
+
+console.log(adi.lastN)
+console.log(adi.firstN)
